dac1a85150b1c44337638ac6029f81b7d6063104
MP <ponolac@gmail.com>
2019-11-17 01:40:31 -0800

    basic operators, math operators, logical operators, typeof operator

--- script.js
+++ script.js
@@ -39,10 +39,37 @@ console.log(
 
 // Variable Mutation
 
-age = 'twenty';
-job = 23;
+age = "twenty";
+job = 23;
 
 alert(job);
 
-var lastName = prompt('what is the last name?');
-console.log(firstName + ' ' + lastName);
+var lastName = prompt("what is the last name?");
+console.log(firstName + " " + lastName);
+
+/****
+ * Basic Operators
+ */
+
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math Operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical Operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+// Typeof Operator
+console.log(typeof johnOlder);
+console.log(typeof ageMark);
