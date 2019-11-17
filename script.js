07fbfc33ce95164c1971a6a5e914fb56792befb7
MP <ponolac@gmail.com>
2019-11-17 01:44:20 -0800

    operator precedence

--- script.js
+++ script.js
@@ -73,3 +73,19 @@ console.log(johnOlder);
 // Typeof Operator
 console.log(typeof johnOlder);
 console.log(typeof ageMark);
+
+// Operator Precedence
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple Operators
+var isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
