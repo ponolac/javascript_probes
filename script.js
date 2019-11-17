5146744d7700b1eecbe9cdcfca91bc6da6ffd6ef
MP <ponolac@gmail.com>
2019-11-17 01:36:06 -0800

    added external js file

---
+++ script.js
@@ -0,0 +1,16 @@
+var firstName = "John";
+console.log(firstName);
+
+var lastName = "Smith";
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "Teacher";
+console.log(job);
+
+// all variable names must start with $ _ or letter.
