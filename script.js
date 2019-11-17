4b0961fb1ba2e1b3f715f4a0531b9a9f92c3fb7d
MP <ponolac@gmail.com>
2019-11-17 01:38:31 -0800

    variable mutation and type coercion

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ job = "Teacher";
 console.log(job);
 
 // all variable names must start with $ _ or letter.
+
+/**********
+ * Variable mutation and type coercion
+ */
+
+var firstName = "John";
+var age = 28; // Type Coercion
+
+console.log(firstName + " " + age); // this works because of Type Coercion. JS converted the age integer into a string.
+var job, isMarried;
+job = "teacher";
+isMarried = false;
+
+console.log(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
