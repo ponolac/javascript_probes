322e87279fef57d99f3f9d741b80f0ee77294f4c
MP <ponolac@gmail.com>
2019-11-18 00:55:13 -0800

    removed white spaces

--- bmrcalc.js
+++ bmrcalc.js
@@ -4,13 +4,10 @@
 
 // The LBM formula for men is: LBM = (0.407 × W) + (0.267 × H) − 19.2
 
-// var userWeight = parseInt(prompt("What is your weight (kg)?"), 10);
-
-// var userHeight = parseInt(prompt("What is your height (cm) ?"), 10);
-
-// var leanBodyMass = 0.252 * userWeight + 0.473 * userHeight - 48.3;
-
-// console.log("Your lean body mass is: " + leanBodyMass + "kg");
+/* var userWeight = parseInt(prompt("What is your weight (kg)?"), 10);
+var userHeight = parseInt(prompt("What is your height (cm) ?"), 10);
+var leanBodyMass = 0.252 * userWeight + 0.473 * userHeight - 48.3;
+console.log("Your lean body mass is: " + leanBodyMass + "kg"); */
 
 // Code refactor below. Made multiple var's easier to read/compact.
 
@@ -23,10 +20,9 @@ console.log("Your lean body mass is: " + leanBodyMass + "kg");
 // BMR = 370 + (9.7976 x LBM) kcal/day, where LBM is lean body mass in pounds.
 
 var basalMetabolicRate = 370 + 21.6 * leanBodyMass;
-
 console.log("Your BMR is " + basalMetabolicRate + " kcal/day");
 
-/* Calculate TDEE by multiplying BMR by an activity factor:
+/* We can calculate TDEE by multiplying BMR by an activity factor:
  *Sedentary* = 1.2
  *Lightly Active* = 1.375
  *Moderately Active* = 1.55
@@ -34,7 +30,5 @@ console.log("Your BMR is " + basalMetabolicRate + " kcal/day");
  *Extremely Active* = 1.9 */
 
 var activityFactor = parseFloat(prompt("What is your activity level?"), 10);
-
 var tDEE = basalMetabolicRate * activityFactor;
-
 console.log("Your TDEE is " + tDEE);
