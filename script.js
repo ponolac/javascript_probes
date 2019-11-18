da17f4fb8cb1814c063b8b7c8187aec2b38b29f6
MP <ponolac@gmail.com>
2019-11-17 19:47:11 -0800

    ifelse statement

--- script.js
+++ script.js
@@ -1,118 +1,126 @@
-var firstName = "John";
-console.log(firstName);
+// var firstName = "John";
+// console.log(firstName);
 
-var lastName = "Smith";
-var age = 28;
+// var lastName = "Smith";
+// var age = 28;
 
-var fullAge = true;
-console.log(fullAge);
+// var fullAge = true;
+// console.log(fullAge);
 
-var job;
-console.log(job);
+// var job;
+// console.log(job);
 
-job = "Teacher";
-console.log(job);
+// job = "Teacher";
+// console.log(job);
 
-// all variable names must start with $ _ or letter.
+// // all variable names must start with $ _ or letter.
 
-/**********
- * Variable mutation and type coercion
- */
+// /**********
+//  * Variable mutation and type coercion
+//  */
 
-var firstName = "John";
-var age = 28; // Type Coercion
+// var firstName = "John";
+// var age = 28; // Type Coercion
 
-console.log(firstName + " " + age); // this works because of Type Coercion. JS converted the age integer into a string.
-var job, isMarried;
-job = "teacher";
-isMarried = false;
+// console.log(firstName + " " + age); // this works because of Type Coercion. JS converted the age integer into a string.
+// var job, isMarried;
+// job = "teacher";
+// isMarried = false;
 
-console.log(
-  firstName +
-    " is a " +
-    age +
-    " year old " +
-    job +
-    ". Is he married? " +
-    isMarried
-);
+// console.log(
+//   firstName +
+//     " is a " +
+//     age +
+//     " year old " +
+//     job +
+//     ". Is he married? " +
+//     isMarried
+// );
 
-// Variable Mutation
+// // Variable Mutation
 
-age = "twenty";
-job = 23;
+// age = "twenty";
+// job = 23;
 
-alert(job);
+// alert(job);
 
-var lastName = prompt("what is the last name?");
-console.log(firstName + " " + lastName);
+// var lastName = prompt("what is the last name?");
+// console.log(firstName + " " + lastName);
 
-/****
- * Basic Operators
- */
+// /****
+//  * Basic Operators
+//  */
 
-var year, yearJohn, yearMark;
-now = 2018;
-ageJohn = 28;
-ageMark = 33;
+// var year, yearJohn, yearMark;
+// now = 2018;
+// ageJohn = 28;
+// ageMark = 33;
 
-// Math Operators
-yearJohn = now - ageJohn;
-yearMark = now - ageMark;
+// // Math Operators
+// yearJohn = now - ageJohn;
+// yearMark = now - ageMark;
 
-console.log(yearJohn);
+// console.log(yearJohn);
 
-console.log(now + 2);
-console.log(now * 2);
-console.log(now / 10);
+// console.log(now + 2);
+// console.log(now * 2);
+// console.log(now / 10);
 
-// Logical Operators
-var johnOlder = ageJohn > ageMark;
-console.log(johnOlder);
+// // Logical Operators
+// var johnOlder = ageJohn > ageMark;
+// console.log(johnOlder);
 
-// Typeof Operator
-console.log(typeof johnOlder);
-console.log(typeof ageMark);
+// // Typeof Operator
+// console.log(typeof johnOlder);
+// console.log(typeof ageMark);
 
-// Operator Precedence
+// // Operator Precedence
 
-var now = 2018;
-var yearJohn = 1989;
-var fullAge = 18;
+// var now = 2018;
+// var yearJohn = 1989;
+// var fullAge = 18;
 
-// Multiple Operators
-var isFullAge = now - yearJohn >= fullAge;
-console.log(isFullAge);
+// // Multiple Operators
+// var isFullAge = now - yearJohn >= fullAge;
+// console.log(isFullAge);
 
-// Grouping
-var ageJohn = now - yearJohn;
-var ageMark = 35;
-var average = (ageJohn + ageMark) / 2;
-console.log(average);
+// // Grouping
+// var ageJohn = now - yearJohn;
+// var ageMark = 35;
+// var average = (ageJohn + ageMark) / 2;
+// console.log(average);
 
-// Multiple Assignment
-var x, y;
-x = y = (3 + 5) * 4 - 6;
-console.log(x, y);
+// // Multiple Assignment
+// var x, y;
+// x = y = (3 + 5) * 4 - 6;
+// console.log(x, y);
 
-// More operators
-// x = x * 2;
-// x *= 2;
-// console.log(x);
-x += 10;
-console.log(x);
-x -= 6;
-console.log(x);
-x -= 35;
-console.log(x);
+// // More operators
+// // x = x * 2;
+// // x *= 2;
+// // console.log(x);
+// x += 10;
+// console.log(x);
+// x -= 6;
+// console.log(x);
+// x -= 35;
+// console.log(x);
 
-x += 1;
-console.log(x);
+// x += 1;
+// console.log(x);
 
 /*********
  * If / Else Statements
- *
  */
 
 var firstName = "John";
 var civilStatus = "single";
+
+// log a string to the console whether he is married or not
+
+if (civilStatus === "married") {
+  console.log(firstName + ' is married');
+} else {
+  console.log(firstName + ' is single');
+}
+
