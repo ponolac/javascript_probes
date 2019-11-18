08e8e31a1f5feb6b1a9ec93cf320ea5914741676
MP <ponolac@gmail.com>
2019-11-18 00:38:18 -0800

    added prompt to store height and weight, saved lean body mass formula in a var, consolelog lbm in kg

--- bmrcalc.js
+++ bmrcalc.js
@@ -3,3 +3,11 @@
 // Calculate lean body mass. Boer formula, for women: LBM = (0.252 × W) + (0.473 × H) − 48.3
 
 // The LBM formula for men is: LBM = (0.407 × W) + (0.267 × H) − 19.2
+
+var userWeight = parseInt(prompt("What is your weight (kg)?"), 10);
+
+var userHeight = parseInt(prompt("What is your height (cm) ?"), 10);
+
+var leanBodyMass = 0.252 * userWeight + 0.473 * userHeight - 48.3;
+
+console.log("Your lean body mass is: " + leanBodyMass + "kg");
