3e85d110dcf795f6aa78d96c2a64015a0a7d1f0f
MP <ponolac@gmail.com>
2019-11-18 00:35:35 -0800

    added Basal Metabolic Rate formula and Lean Body Mass formula

---
+++ bmrcalc.js
@@ -0,0 +1,5 @@
+// BMR calculator - Katch-McArdle Formula
+
+// Calculate lean body mass. Boer formula, for women: LBM = (0.252 × W) + (0.473 × H) − 48.3
+
+// The LBM formula for men is: LBM = (0.407 × W) + (0.267 × H) − 19.2
