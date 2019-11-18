0de58412c108d23343fd6cc01b2cea11ae724039
MP <ponolac@gmail.com>
2019-11-18 00:08:26 -0800

    started building body mass index calculator

---
+++ 1stchallenge.js
@@ -0,0 +1,9 @@
+// bmi formula: mass / height^2 = mass / (height * height).
+
+// store mark and john's mass (kg) and height (meters) in variables
+
+var markMass, johnMass, markHeight, johnHeight;
+markMass = 75;
+johnMass = 81;
+markHeight = 1.79;
+johnHeight = 1.92;
