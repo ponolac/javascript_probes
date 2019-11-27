1cde3e4efa41054e415f686c60a6031593d852e0
MP <ponolac@gmail.com>
2019-11-26 17:25:43 -0800

    added requirements as comments

--- 3rdchallenge.js
+++ 3rdchallenge.js
@@ -1,1 +1,13 @@
 // Tip Calculator
+
+// create an array with the tips
+
+// create another array with tips plus bill
+
+// create a function that calculates the tip
+
+// to calculate 20% of a value, multiply the value by 0.2
+
+// Tip 20% of bill when bill is < $50
+// Tip 15% of bill when bill is between $50-$200
+// Tip 10% of bill when bill is > $200
