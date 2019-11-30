b09a2828ee0917e9890947bce5605ddc578a0e06
MP <ponolac@gmail.com>
2019-11-30 03:34:39 -0800

    log to the console who has the highest BMI using if-else.

--- 4thchallenge.js
+++ 4thchallenge.js
@@ -3,6 +3,7 @@
 // Add a method to each object to calculate the BMI.
 // Save the BMI to the object
 // Also, return the BMI from the method
+// BMI = mass / height^2 = mass / (height * height). (mass in KG and height in Meter).
 
 var mark = {
   fullName: "Mark Johnson",
@@ -20,12 +21,26 @@ console.log(mark);
 var john = {
   fullName: "John Mead",
   mass: 81,
-  height: 1.92
+  height: 1.92,
+  calcBMI: function() {
+    // return this.mass / (this.height * this.height);
+    this.johnBMI = this.mass / (this.height * this.height);
+  }
 };
 
-// john.calcBMI();
+john.calcBMI();
 console.log(john);
 
 // Log to the console who has the highest BMI. They might have the same BMI.
 
-// BMI = mass / height^2 = mass / (height * height). (mass in KG and height in Meter).
+if (mark.markBMI > john.johnBMI) {
+  console.log(
+    "Mark's BMI is higher than John's BMI! Mark's BMI is " + mark.markBMI
+  );
+} else if (mark.markBMI < john.johnBMI) {
+  console.log(
+    "John's BMI is higher than Mark's BMI! John's BMI is " + john.johnBMI
+  );
+} else {
+  console.log("Both Mark and John have the same BMI.");
+}
