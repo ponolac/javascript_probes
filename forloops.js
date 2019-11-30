f0485eedc017d779fb056ce75f919b2aec7ccd77
MP <ponolac@gmail.com>
2019-11-30 14:48:14 -0800

    created a js file titled for loops. added some comments and tested a basic for loop.

---
+++ forloops.js
@@ -0,0 +1,12 @@
+// FOR LOOPS
+
+/* for loops have 3 parts: 
+-- initial value of a counter, 
+-- a condition that is evaluated before each loop iteration,
+-- a counter update after each iteration. */
+
+// print a number 0-9
+
+for (var i = 0; i < 10; i++) {   // i++ is like saying i = i+1
+    console.log(i);
+}
