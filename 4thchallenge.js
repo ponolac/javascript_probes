69e3b71cb05b5f612aa463ac0d68af916409f486
MP <ponolac@gmail.com>
2019-11-30 02:29:05 -0800

    created new file to practice using objects and methods. Added the initial requirements for the practice challenge.

---
+++ 4thchallenge.js
@@ -0,0 +1,13 @@
+// Using objects and methods. 
+
+// Create an object for Mark and John with properties for their full name, mass, and height.
+
+// Add a method to each object to calculate the BMI.
+
+// Save the BMI to the object
+
+// Also, return the BMI from the method
+
+// Log to the console who has the highest BMI. They might have the same BMI.
+
+// BMI = mass / height^2 = mass / (height * height). (mass in KG and height in Meter).
